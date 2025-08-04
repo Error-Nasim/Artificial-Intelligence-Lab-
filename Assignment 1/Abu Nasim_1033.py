e6290d91230402687cbf2afe4481baadf859b7b1
Abu Nasim <abu_nasim1033@uits.edu.bd>
2025-08-05 00:06:04 +0600

    Create Abu Nasim_1033.py

---
+++ Assignment 1/Abu Nasim_1033.py
@@ -0,0 +1,48 @@
+# You Can Find The Truth In My Code*_*
+
+name="Abu Nasim" # MY NAME 
+no_name=name.replace(" ","") # get rid of any spaces in the name
+
+# Printing My name
+print("Full Name: ",name)
+
+# Vowel And Consonant Count
+vowel="aeiouAeiou"
+vwl=0
+con=0
+for l in no_name:
+    if l in vowel:
+        vwl=vwl+1
+    else:
+        con=con+1
+print(f"Vowels: {vwl}, Consonants: {con}")
+
+# Asci Value Count
+asci_value = []
+for l in name:
+    asci_value.append(ord(l))  #ord function gives the ascii value of a char 
+print(f"ASCII Values : {asci_value}")
+
+#Reverse Name 
+rev_name=""
+for char in name:
+    rev_name=char+rev_name
+print(f"Reversed Name: {rev_name}")
+
+#Cheking Pelindrom
+if name==rev_name:
+    print("Is Palindrom: True")
+else:
+    print("Is Palindrom: False")
+
+# Unique Letters In My Name
+unq_let=sorted(list(set(no_name))) #first stored in a set then convert it to a list then sort it
+print(f"Unique Letters (sorted): {unq_let}")
+
+# First Non Repeting letters
+c=''
+for l in no_name:
+    if no_name.count(l)==1:
+        c=l
+        break
+print(f"First Non-Repeating Character: {c}")
